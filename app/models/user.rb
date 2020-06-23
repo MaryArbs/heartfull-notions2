76a04d18bdcfa4d41ebb997adadde393d9659749
MaryArbs <mary.arbachauskas@gmail.com>
2020-06-23 16:32:23 -0400

    add user/comment relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+    has_many :posts
+    has_many :comments  # has many comments through posts?
+    has_secure_password
+    validates :username,
+              uniqueness: { message: 'is already in use. Try again!' },
+              length: { minimum: 2 },
+              length: { maximum: 30 }
+    validates :email, uniqueness: { message: 'is already in use. Try again!' }
+    validates_presence_of :username, :email, :password
+end
 end
