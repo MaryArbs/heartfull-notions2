8439deb5119dc96897457d0639de5dd9fd9f46a1
MaryArbs <mary.arbachauskas@gmail.com>
2020-06-23 16:30:12 -0400

    update post validations/relations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+    belongs_to :user
+    has_many :comments, dependent: :destroy 
+    # dependent: :destroy means the comments related 
+    # to the specific post in mention get deleted if the post does.
 end
