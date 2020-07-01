7c50df51a12709dc50d3e30c08c46fda25f40755
MaryArbs <mary.arbachauskas@gmail.com>
2020-07-01 11:48:26 -0400

    add comments to resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  root 'posts#index'
-  resources :posts
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :posts do
+  	resources :comments
+  end
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'posts#index'
 end
