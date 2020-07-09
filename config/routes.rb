ca74821064ea9033a95e192ac712b4b8f451f287
MaryArbs <mary.arbachauskas@gmail.com>
2020-07-09 13:35:37 -0400

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'posts#index'
+
   resources :posts do
   	resources :comments
   end
-  
-  get 'signup'  => 'users#new 
-  resources :users
-  root 'posts#index'
+ 
+
+  get '/signup', to: 'users#new', as: 'signup'
+  post '/signup', to: 'users#create'
+  delete '/logout', to: 'sessions#destroy', as: 'logout'
 end
