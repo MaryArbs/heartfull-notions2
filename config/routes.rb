49141a8beeb7356598c6ad356dad42c2c8f94dae
MaryArbs <mary.arbachauskas@gmail.com>
2020-07-10 15:06:10 -0400

    add login feature

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,7 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new', as: 'signup'
   post '/signup', to: 'users#create'
   delete '/logout', to: 'sessions#destroy', as: 'logout'
+
+  get '/login', to: 'sessions#new', as: 'login'
+  post '/login', to: 'sessions#create'
 end
