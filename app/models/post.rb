ce7eb138c630d32d07f8d721b4fefdfc19605e3a
MaryArbs <mary.arbachauskas@gmail.com>
2020-07-14 14:28:20 -0400

    add validations to post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
     belongs_to :user
     has_many :comments, dependent: :destroy 
+    validates_presence_of :title, :content
     # dependent: :destroy means the comments related 
     # to the specific post in mention get deleted if the post does.
     # has_attached_file :image, styles: { large: "600x600>" medium: "300x300>", thumb: "150x150#" }
