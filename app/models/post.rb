ca3dfa3e7b043d491782a52ff39cfef7a38669ee
MaryArbs <mary.arbachauskas@gmail.com>
2020-07-15 16:20:35 -0400

    update validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,4 @@
 class Post < ApplicationRecord
-    belongs_to :user
     has_many :comments, dependent: :destroy 
     validates_presence_of :title, :content
     # dependent: :destroy means the comments related 
